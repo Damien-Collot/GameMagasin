fb0a79011ddc44e53662c4faa4baa4971b7949f0
Damien-Collot <dcollot76@gmail.com>
2022-11-21 10:13:25 +0100

    Feat : init GameMagasin

---
+++ server.js
@@ -0,0 +1,17 @@
+import express, { json, request, response } from "express";
+import mongoose from "mongoose";
+
+const app = express()
+
+app.use(json())
+
+mongoose.connect("mongodb://localhost:27017/Magasin").then((e) => {
+  console.log("Connected")
+})
+.catch((e) => {
+  console.log("failed")
+})
+
+app.listen(3000, () => {
+    console.log(`Server Started at http://localhost:${3000}`)
+  })
