0cfb4d7e23148a4320a77923139faae490e38bfe
Nicolas Wrobel <nicolas.18.wrobel@gmail.com>
2022-11-21 17:45:22 +0100

    fix : Création de db

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import Games from './games.json' assert {type: 'json'}
 
-mongoose.connect("mongodb://127.0.0.1:27017/Magasin")
+mongoose.connect("mongodb://127.0.0.1:27017/Magasin",function(){
+    /* Drop the DB */
+    mongoose.connection.db.dropDatabase();
+});
 
 const Game = mongoose.model("Game", { 
     name :  String,
