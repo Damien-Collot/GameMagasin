53cd700edadb3d60e62a3f3cf6dda301d9870dbe
Damien-Collot <dcollot76@gmail.com>
2023-01-29 21:49:43 +0100

    Mise à jour back.js

--- back/server.js
+++ back/server.js
@@ -1,8 +1,9 @@
 import express, { json, request, response } from "express";
 import mongoose from "mongoose";
+import cors from 'cors'
 
 const app = express()
-
+app.use(cors())
 app.use(json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/Magasin").then((e) => {
